d73a578931134a51af7f0460caf8d172c7fb91fb
Julia Tsukerman <julia.tsuk@gmail.com>
2021-11-01 00:06:38 +0300

    model

--- UserInfoApp/Models/UserData.swift
+++ UserInfoApp/Models/UserData.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+struct User{
+    var userName: String
+    var userPassword: String
+    var type: HobbyType
+}
+enum HobbyType: String {
+    case reading
+    case walking
+    case working
+    var definition: String {
+        switch self {
+    case.reading:
+        return "I like reading fiction and non fiction literature."
+    case.walking:
+        return "Walking is a great way to relax"
+    case.working:
+        return "My work is very intersting, I'm a part of IVF team"
+            }
+    }
+}
+let A = User(userName: "A", userPassword: "1234", type: .reading)
+    
